6ae13c924eb358cc3726b3b3256f4ce595abaee4
Han Bao <baaaohaaan@outlook.com>
2022-08-02 23:46:10 -0700

    Add unit testing for decode function

--- frontend/src/test.py
+++ frontend/src/test.py
@@ -1,5 +1,8 @@
+import hashlib
+import os
+from PIL import Image
 import unittest
-
+from main import decode
 from main import validate_password
 
 class Testing(unittest.TestCase):
@@ -15,6 +18,19 @@ class Testing(unittest.TestCase):
         self.assertEqual(validate_password('5we13aaaa12345'), 3)
         self.assertEqual(validate_password('AAAaaaa12345'), -1)
         self.assertEqual(validate_password('kjs36adfjIS230Rcv5D'), -1)
+    
+    def test_decode(self):
+        script_dir = os.path.dirname(__file__)
+        rel_path = '../../test.png'
+        abs_img_path = os.path.join(script_dir, rel_path)
+        image_to_decode = Image.open(abs_img_path)
+
+        self.assertEqual(decode(image_to_decode, ''), False)
+        self.assertEqual(decode(image_to_decode, 'osdfjlj32'), False)
+        self.assertEqual(decode(image_to_decode, 'Abcd1234'), False)
+        
+        password = hashlib.sha256('Abcd1234'.encode()).hexdigest()
+        self.assertEqual(decode(image_to_decode, password), 'xyz')
 
 if __name__ == '__main__':
     unittest.main()
